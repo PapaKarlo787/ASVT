80d80914578f9860011d722f86c0da04583a2f16
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2023-03-02 15:10:42 +0300

    tmp troubles

--- 1/code.asm
+++ 1/code.asm
@@ -1,30 +1,63 @@
 org 0x7c00
-res=2
+res=18
 include "H_FAT.asm"
 
 
-
-xor dx, dx
-mov ax, 0x0201
-mov cx, 2
-mov bx, 0x500
-int 13h
-jmp 0x500
+	mov cx, [0x7c0e]	; reserved
+	mov bx, 0x500		; datapointer
+	mov ax, 1			; logical sector for booting
+lp_res:
+	pusha
+	mov cx, 18
+	xor dx, dx
+	div cx
+	inc dx				; sector at track
+	mov cx, ax
+	mov ax, dx
+	xor dx, dx
+	shr cx, 1
+	jnc zhead
+	mov dh, 1
+zhead:
+	shl cx, 6
+	add cx, ax
+	mov ax, 0x0201
+	int 13h
+	popa
+	add bh, 0x2
+	inc ax
+	loop lp_res
+	jmp l
 
 
 
 times 510-($-$$) db 0
 db 0x55, 0xaa	
 
-
-
-mov ax, [k]
-int 10h
-jmp $
+org 0x500
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+times 512 db 0
+l:
+	mov ax, [k]
+	int 10h
+	jmp $
 k:
-dw 3
-
-
+	dw 2
 times 512*res-($-$$) db 0
 dd 0x0ffffff0, 0x0fffffff, 0x0ffffff8
 times 1440*1024-($-$$) db 0
